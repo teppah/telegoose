f9cc0c88ce2923fd2fb39658f9272ec624b26984
Yi Feng Yang <yifeng@yfyang.dev>
2021-04-30 20:43:19 -0400

    Add placeholders for FSM states

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,32 @@
+use teloxide::prelude::*;
 
-#[derive(Transitiion, From)]
+#[derive(Transition, Clone)]
 pub enum Dialogue {
-
+    Start(StartState),
+    ReceiveFile(ReceiveFileState),
+    ReceiveFormat(ReceiveFormatState),
 }
+
+impl Default for Dialogue {
+    fn default() -> Self {
+        Self::Start(StartState)
+    }
+}
+
+#[derive(Clone)]
+pub struct StartState;
+
+#[teloxide(subtransition)]
+async fn start(state: StartState, cx: TransitionIn<AutoSend<Bot>>) -> TransitionOut<Dialogue> {}
+
+#[derive(Clone)]
+pub struct ReceiveFileState;
+
+#[teloxide(subtransition)]
+async fn receive_file(state: ReceiveFileState, cx: TransitionIn<AutoSend<Bot>>) -> TransitionOut<Dialogue> {}
+
+#[derive(Clone)]
+pub struct ReceiveFormatState;
+
+#[teloxide(subtransition)]
+async fn receive_format(state: ReceiveFormatState, cx: TransitionIn<AutoSend<Bot>>) -> TransitionOut<Dialogue> {}
